c78594ab835be1f6a8b1e6b1454da5a8fe5f75c1
Kyle Vosper <kvosper@gmail.com>
2019-01-18 16:51:54 +0000

    Initial implementation

---
+++ components/common/src/main/java/com/hotels/styx/api/metrics/WhitelistMetricRegistry.java
@@ -0,0 +1,166 @@
+package com.hotels.styx.api.metrics;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistryListener;
+import com.codahale.metrics.Timer;
+import com.hotels.styx.api.MetricRegistry;
+
+import java.util.List;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.regex.Pattern;
+
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * A metric registry that only allows metrics to be registered if they are on a whitelist.
+ *
+ * This allows us to have one centralised "source-of-truth" to list all of the metrics.
+ */
+public class WhitelistMetricRegistry implements MetricRegistry {
+    private final MetricRegistry delegate;
+    private final List<Pattern> whitelist;
+
+    public WhitelistMetricRegistry(MetricRegistry delegate, List<String> whitelist) {
+        this(whitelist.stream().map(Pattern::compile).collect(toList()), delegate);
+    }
+
+    private WhitelistMetricRegistry(List<Pattern> whitelist, MetricRegistry delegate) {
+        this.delegate = requireNonNull(delegate);
+        this.whitelist = requireNonNull(whitelist);
+    }
+
+    private boolean isPermittedMetric(String name) {
+        // TODO find optimal implementation
+
+        return whitelist.stream().anyMatch(pattern ->
+                pattern.matcher(name).matches());
+    }
+
+    private void rejectMetricIfNotWhitelisted(String name) {
+        if (!isPermittedMetric(name)) {
+            throw new IllegalArgumentException("Metric '" + name + "' is not registered in the whitelist");
+        }
+    }
+
+    @Override
+    public MetricRegistry scope(String name) {
+        return delegate.scope(name);
+    }
+
+    @Override
+    public <T extends Metric> T register(String name, T metric) throws IllegalArgumentException {
+        rejectMetricIfNotWhitelisted(name);
+
+        return delegate.register(name, metric);
+    }
+
+    @Override
+    public boolean deregister(String name) {
+        return delegate.deregister(name);
+    }
+
+    @Override
+    public Counter counter(String name) {
+        rejectMetricIfNotWhitelisted(name);
+
+        return delegate.counter(name);
+    }
+
+    @Override
+    public Histogram histogram(String name) {
+        rejectMetricIfNotWhitelisted(name);
+
+        return delegate.histogram(name);
+    }
+
+    @Override
+    public Meter meter(String name) {
+        rejectMetricIfNotWhitelisted(name);
+
+        return delegate.meter(name);
+    }
+
+    @Override
+    public Timer timer(String name) {
+        rejectMetricIfNotWhitelisted(name);
+
+        return delegate.timer(name);
+    }
+
+    @Override
+    public void addListener(MetricRegistryListener listener) {
+        delegate.addListener(listener);
+    }
+
+    @Override
+    public void removeListener(MetricRegistryListener listener) {
+        delegate.removeListener(listener);
+    }
+
+    @Override
+    public SortedSet<String> getNames() {
+        return delegate.getNames();
+    }
+
+    @Override
+    public SortedMap<String, Gauge> getGauges() {
+        return delegate.getGauges();
+    }
+
+    @Override
+    public SortedMap<String, Gauge> getGauges(MetricFilter filter) {
+        return delegate.getGauges(filter);
+    }
+
+    @Override
+    public SortedMap<String, Counter> getCounters() {
+        return delegate.getCounters();
+    }
+
+    @Override
+    public SortedMap<String, Counter> getCounters(MetricFilter filter) {
+        return delegate.getCounters(filter);
+    }
+
+    @Override
+    public SortedMap<String, Histogram> getHistograms() {
+        return delegate.getHistograms();
+    }
+
+    @Override
+    public SortedMap<String, Histogram> getHistograms(MetricFilter filter) {
+        return delegate.getHistograms(filter);
+    }
+
+    @Override
+    public SortedMap<String, Meter> getMeters() {
+        return delegate.getMeters();
+    }
+
+    @Override
+    public SortedMap<String, Meter> getMeters(MetricFilter filter) {
+        return delegate.getMeters(filter);
+    }
+
+    @Override
+    public SortedMap<String, Timer> getTimers() {
+        return delegate.getTimers();
+    }
+
+    @Override
+    public SortedMap<String, Timer> getTimers(MetricFilter filter) {
+        return delegate.getTimers(filter);
+    }
+
+    @Override
+    public SortedMap<String, Metric> getMetrics() {
+        return delegate.getMetrics();
+    }
+}
